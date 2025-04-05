647935f578dcdb33a593e6b8f97c691239ad8225
rbtutorials <ryanbalieiro@icloud.com>
2025-04-05 13:57:41 -0300

    - Vite: fixed base path.

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-resume-template/',
+  base: '/alana-richard-vue-resume/',
   plugins: [vue()],
   css: {
     preprocessorOptions: {
